3846f850d931ec3968f0630ec6b5f684fbc1c028
mycahdt <mycahdt@udel.edu>
2024-11-01 20:38:10 -0400

    Configuration A

---
+++ scipt.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.textContent = 'Button was clicked!';
+});
